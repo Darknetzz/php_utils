004a1d9c5a62f9486b6d8570997d649c582162bd
Kristian Røste <github@roste.org>
2023-10-26 14:50:07 +0200

    added file methods

--- PHPUtils/Files.php
+++ PHPUtils/Files.php
@@ -5,9 +5,9 @@
 /* ────────────────────────────────────────────────────────────────────────── */
 class Files extends Base {
 
-    function __construct() {
-        $this->debug("Files included");
-    }
+    // function __construct() {
+    //     $this->debug("Files included");
+    // }
 
     public function include_folder(string $fullpath, array $except) {
         
@@ -15,6 +15,33 @@ class Files extends Base {
         //     $this->error("Unable to include path $fullpath");
         // }
 
+    }
+
+    public function file_read(string $fullpath) {
+        if (file_exists($fullpath) && is_file($fullpath)) {
+            $f = fopen($fullpath, 'r');
+            return $f->file_read();
+        }
+    }
+
+    public function file_write(string $fullpath, string $content, bool $create = true) {    
+        if (!file_exists($fullpath) && $create === true) {
+            touch($fullpath);
+        }
+        
+        $f = fopen($fullpath, 'w+');
+        $f->file_write($content);
+        $f->close();
+    }
+
+    # A "file_close" function should not be neccessary as both
+    # file_read and file_write properly closes the file, but just in case
+    public function file_close(mixed $handle) {
+        if (!is_resource($handle)) {
+            return false;
+        }
+        $handle->close();
+        return true;
     }
 }
 
