7830d17898a1086bb4bf3f8baff592e9618c254e
Kristian Røste <github@roste.org>
2023-11-09 09:16:03 +0100

    function ipInRange

--- PHPUtils/Network.php
+++ PHPUtils/Network.php
@@ -1,12 +1,29 @@
 <?php
 class Network extends Base {
-
-    function cidrToRange($cidr) {
+    
+    /**
+     * cidrToRange
+     *
+     * @param  mixed $cidr
+     * @return array
+     */
+    function cidrToRange(string $cidr) {
         $range = array();
         $cidr = explode('/', $cidr);
         $range[0] = long2ip((ip2long($cidr[0])) & ((-1 << (32 - (int)$cidr[1]))));
         $range[1] = long2ip((ip2long($range[0])) + pow(2, (32 - (int)$cidr[1])) - 1);
         return $range;
+      }
+
+      
+      function ipInRange(string $ip, string $lowerip, string $upperip) {
+        $lower_long = ip2long($lowerip);
+        $upper_long = ip2long($upperip);
+
+        if ($lower_long <= $ip && $ip >= $upper_long) {
+            return true;
+        }
+        return false;
       }
 
 }
