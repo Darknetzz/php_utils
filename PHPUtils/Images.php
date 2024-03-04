a630471998a60d8d34018627cfe749d07f303275
Kristian <github@roste.org>
2024-03-04 12:59:38 +0100

    Update image manipulation functions

--- PHPUtils/Images.php
+++ PHPUtils/Images.php
@@ -15,15 +15,54 @@ class Images extends Base {
     /* ────────────────────────────────────────────────────────────────────────── */
     /*                                 Blur image                                 */
     /* ────────────────────────────────────────────────────────────────────────── */
-    function blur(string $imagePath, float $radius = 5, float $sigma = 3, int $channel = 0) {
-        header('Content-type: image/jpeg');
+    function blur(string $imagePath, float $radius = 10, float $sigma = 25, int $channel = imagick::CHANNEL_ALL) {
+        // header('Content-type: image/jpeg');
 
-        if (!$image->pingImage($imagePath)) {
-            die("Invalid image file: $imagePath");
+        try {
+
+            // Create a temporary file
+            $tmpDir          = dirname($imagePath);
+            $tmpFile         = "blurred_".basename($imagePath);
+            $tmpFullPath     = $tmpDir."/".$tmpFile;
+            $tmpRelativePath = basename($tmpDir)."/".$tmpFile;
+
+            if (is_file($tmpFullPath)) {
+                return $tmpRelativePath;
+            }
+
+            $image = new Imagick($imagePath);
+            $image->blurImage($radius, $sigma, $channel);
+            $image->writeImage($tmpFullPath);
+
+            // Schedule the file deletion when the script execution is finished
+            register_shutdown_function(function() use ($tmpFullPath, $image) {
+                $image->clear();
+                $image->destroy();
+                unlink($tmpFullPath);
+            });
+
+            return $tmpRelativePath; // return the path of the temporary file
+        } catch (ImagickException $e) {
+            die("Error: " . $e->getMessage());
         }
+    }
+
+    /* ────────────────────────────────────────────────────────────────────────── */
+    /*                                 textToImage                                */
+    /* ────────────────────────────────────────────────────────────────────────── */
+    function textToImage(string $text, string $imagePath, string $fontPath, int $fontSize, int $x, int $y, string $color = 'black') {
+        $draw = new ImagickDraw();
+        $draw->setFont($fontPath);
+        $draw->setFontSize($fontSize);
+        $draw->setFillColor($color);
+        $draw->setGravity(Imagick::GRAVITY_NORTHWEST);
+        $draw->annotation($x, $y, $text);
+
         $image = new Imagick($imagePath);
-        $image->blurImage(5,3);
-        echo $image;
+        $image->drawImage($draw);
+        $image->writeImage($imagePath);
+        $image->clear();
+        $image->destroy();
     }
 }
 ?>
