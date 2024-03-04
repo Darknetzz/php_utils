b2334652f1fd4bc3d94bc41cad6f240a73411a72
Kristian <github@roste.org>
2024-03-04 10:47:18 +0100

    Add .history to .gitignore

---
+++ PHPUtils/Images.php
@@ -0,0 +1,29 @@
+<?php
+
+/* ────────────────────────────────────────────────────────────────────────── */
+/*                                   IMAGES                                   */
+/* ────────────────────────────────────────────────────────────────────────── */
+class Images extends Base {
+    // Class implementation goes here
+    public function __construct() {
+        // Initialization code goes here
+        if (!extension_loaded('imagick')) {
+            die("Imagick extension is not enabled. To use the class `Images` you must have Imagick extension enabled.");
+        }
+    }
+
+    /* ────────────────────────────────────────────────────────────────────────── */
+    /*                                 Blur image                                 */
+    /* ────────────────────────────────────────────────────────────────────────── */
+    function blur(string $imagePath, float $radius = 5, float $sigma = 3, int $channel = 0) {
+        header('Content-type: image/jpeg');
+
+        if (!$image->pingImage($imagePath)) {
+            die("Invalid image file: $imagePath");
+        }
+        $image = new Imagick($imagePath);
+        $image->blurImage(5,3);
+        echo $image;
+    }
+}
+?>
